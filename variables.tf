afe4a4a7115fb41d3c79625422a42167169ec8e2
Alex Johnston <alex-johnston@live.co.uk>
2024-10-17 13:43:15 +0100

    feat: first commit, deploy doesn't quite work

--- variables.tf
+++ variables.tf
@@ -1,3 +1,13 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
+data "aws_partition" "current" {}
+
+data "aws_secretsmanager_secret_version" "github_token" {
+  secret_id = "github-token"
+}
+
 variable "vpc_name" {
   description   = "The name of the VPC"
   type          = string
